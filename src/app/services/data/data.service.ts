5327a92cc8594249a24589a37a14693f540c0812
yotam <ynagor@gmail.com>
2021-10-27 23:55:42 +0300

    http => https

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -9,22 +9,22 @@ import { City , CurrentCityData , FutureForecast } from '../../pages/weather-inf
 export class DataService {
   constructor(private http: HttpClient) { }
   getCitiesData(apiKey: string, filterValue: string): Observable<City[]> {
-    const url = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${filterValue}`;
+    const url = `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${filterValue}`;
     return this.http.get<any>(url);
   }
 
   getCityByCurrentLocationData(apiKey: string, lat: number, lon: number): Observable<any> {
-    const url = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${lat},${lon}`;
+    const url = `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${lat},${lon}`;
     return this.http.get<any>(url);
   }
 
   getCurrentCityCondition(apiKey: string, cityId: string): Observable<CurrentCityData> {
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/${cityId}?apikey=${apiKey}`;
+    const url = `https://dataservice.accuweather.com/currentconditions/v1/${cityId}?apikey=${apiKey}`;
     return this.http.get<any>(url);
   }
 
   getFutureForecast(apiKey: string, cityId: string, isCelsius: boolean): Observable<FutureForecast[]> {
-    const url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityId}?apikey=${apiKey}&metric=${isCelsius}`;
+    const url = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityId}?apikey=${apiKey}&metric=${isCelsius}`;
     return this.http.get<any>(url);
   }
 }
